73731c5d676ce4ad23fdea9bb901a4325d0ecc36
Tim Gromeyer <sakul8826@gmail.com>
2023-03-24 17:11:10 +0100

    Rename: `packaging/` --> `include`

--- packaging/build.sh
+++
@@ -1,9 +0,0 @@
-cd "${0%/*}"
-cd ../../build-MarkdownEdit-Desktop-Release/
-sudo cmake --install . --prefix ~/qtprojegt/MarkdownEdit/packaging/MarkdownEdit/usr/
-# mkdir -p MarkdownEdit/usr/bin
-# cp ../../build-MarkdownEdit-Desktop-Release/markdownedit MarkdownEdit/usr/local/bin/markdownedit
-# chmod +x MarkdownEdit/usr/local/bin/markdownedit
-cd "${0%/*}"
-dpkg-deb --build MarkdownEdit
-sudo dpkg -i MarkdownEdit.deb
