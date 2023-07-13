5dd7d23fccec3897c9a6a28f003e5eaae369a443
Tim Gromeyer <tim.gromeyer@gmail.com>
2023-07-13 14:33:42 +0200

    Try using cpack instead

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 mkdir -p build && cd build # create build directory
 cmake .. -DCMAKE_BUILD_TYPE=Release # create Makefile
 cmake --build . --config Release -j4 # build project
-cmake --build . --config Release --target package # package
+# cmake --build . --config Release --target package # package
+cpack .
 
