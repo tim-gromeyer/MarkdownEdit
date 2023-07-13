f0d5e64926c89f63b50e1256af56de15b9695366
Tim Gromeyer <tim.gromeyer@gmail.com>
2023-07-13 14:22:27 +0200

    Try to fix release workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,5 @@
-# change directory to the scripts parent directory
-cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
-
-# clone dependencies
-git submodule update --init --recursive -j 3 --depth=1
-
 mkdir -p build && cd build # create build directory
 cmake .. -DCMAKE_BUILD_TYPE=Release # create Makefile
 cmake --build . --config Release -j4 # build project
-cmake --build . --config Release --target package
+cmake --build . --config Release --target package # package
+
