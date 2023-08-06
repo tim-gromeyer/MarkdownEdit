2cab2edf1471a93a7f7eb0bf6b90481ddb5e4104
Tim Gromeyer <tim.gromeyer@gmail.com>
2023-08-06 17:01:49 +0200

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,48 @@
-mkdir -p build && cd build # create build directory
-cmake .. -DCMAKE_BUILD_TYPE=Release # create Makefile
-cmake --build . --config Release -j4 # build project
-# cmake --build . --config Release --target package # package
-cpack .
+#!/bin/bash
 
+CLEAN_BUILD=false
+
+# Check for command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --clean)
+            CLEAN_BUILD=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $key"
+            exit 1
+            ;;
+    esac
+done
+
+# Clean build directory if specified
+if [ "$CLEAN_BUILD" = true ]; then
+    if [ -d "build" ]; then
+        echo "Removing previous build directory..."
+        rm -r build
+    fi
+fi
+
+# Create and navigate to the build directory
+mkdir -p build && cd build
+
+# Configure the project with CMake
+cmake .. -DCMAKE_BUILD_TYPE=Release
+
+# Build the project using all available CPU cores
+cmake --build . --config Release
+
+# Package the project
+cmake --build . --config Release --target package
+
+# Build a rpm package if the rpm command is available
+if command -v rpm &> /dev/null; then
+    echo "Building RPM package..."
+    cpack -G RPM
+fi
+
+# Navigate back to the original directory
+cd ..
