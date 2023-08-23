68d31a332ce7d936d55401e9113549d3ec177f67
Tim Gromeyer <tim.gromeyer@gmail.com>
2023-08-23 23:20:23 +0200

    Update Flatpak build script

--- scripts/build-flatpak.sh
+++ scripts/build-flatpak.sh
@@ -1,3 +1,18 @@
-flatpak-builder build-flatpak include/flatpak/com.github.tim_gromeyer.MarkdownEdit.yml --force-clean
-flatpak build-export export build-flatpak
-flatpak build-bundle export MarkdownEdit.flatpak com.github.tim_gromeyer.MarkdownEdit
+# Set variables for paths and filenames
+APP_ID="com.github.tim_gromeyer.MarkdownEdit"
+BUILD_DIR="build-flatpak"
+EXPORT_DIR="export-flatpak"
+EXPORT_FILENAME="MarkdownEdit.flatpak"
+
+# Clean the build directory
+printf "\nBuilding...\n----------------------------\n\n\n"
+flatpak-builder "$BUILD_DIR" "include/flatpak/$APP_ID.yml" --force-clean
+
+# Export the package
+printf "\nExporting...\n----------------------------\n\n\n"
+flatpak build-export "$EXPORT_DIR" "$BUILD_DIR"
+
+# Bundle the Flatpak package
+printf "\nBundeling...\n----------------------------\n\n\n"
+flatpak build-bundle "$EXPORT_DIR" "$EXPORT_FILENAME" "$APP_ID"
+
