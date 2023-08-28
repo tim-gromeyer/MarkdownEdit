70f3a8dd96480c653836bca6ecf6ff66bc184ecf
Tim Gromeyer <tim.gromeyer@gmail.com>
2023-08-28 11:35:55 +0200

    Rename flatpak app: `com.github` -> `io.github`

--- scripts/build-flatpak.sh
+++ scripts/build-flatpak.sh
@@ -1,5 +1,5 @@
 # Set variables for paths and filenames
-APP_ID="com.github.tim_gromeyer.MarkdownEdit"
+APP_ID="io.github.tim_gromeyer.MarkdownEdit"
 BUILD_DIR="build-flatpak"
 EXPORT_DIR="export-flatpak"
 EXPORT_FILENAME="MarkdownEdit.flatpak"
@@ -20,4 +20,3 @@ flatpak build-export "$EXPORT_DIR" "$BUILD_DIR"
 # Bundle the Flatpak package
 printf "\nBundeling...\n----------------------------\n\n\n"
 flatpak build-bundle "$EXPORT_DIR" "$EXPORT_FILENAME" "$APP_ID"
-
